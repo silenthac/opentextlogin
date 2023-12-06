10f624fadfa81c0cc39ccb5c0ee5b7fcb213c037
Prashant <pundirprashant545@gmail.com>
2023-12-07 00:06:27 +0530

    LoginComplete

--- src/Login.js
+++ src/Login.js
@@ -54,37 +54,41 @@ const Login = () => {
         setPassword('')
     }
   return (
-    <div className='mt-20 '>
-        <div className='w-full max-w-xl mx-auto border border-black bg-gradient-to-l from-blue-900 z-10'>
-            <p className='m-2'>Opentext Administration  Client</p>
-            <p className='m-10'><b>opentext</b><sup>TM</sup>|Administation Client{" "}{" "}<sub>CE 24.1</sub></p>
-
+    <div className='  h-screen' style={{background:"linear-gradient(45deg, rgb(9, 14, 44) 0%, rgb(18, 44, 105) 58.93%, rgb(7, 141, 179) 100%)"}}>
+        <div className='w-full  max-w-xl mx-auto  pt-20  my-auto border-black  '>
+            <p className='m-2  ml-20 text-3xl font-bold text-white'>Opentext Administration  Client</p>
+            <div className='flex justify-center items-center'>
+            <p className='m-10  ml-20 text-white text-md font-bold'><b>Opentext</b><sup>TM </sup>&nbsp;|&nbsp;Administation Client{" "}{" "}<sub>CE 24.1</sub></p>
+            </div>
             <div className='flex justify-evenly p-5 '>
-            <p>Server</p>
+            <p className='text-white text-md font-bold text-m'>Server</p>
             <input  className='border-black  px-3 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40' type='text' value={IPaddress} onChange={(e)=>{setIPaddress(e.target.value)}}/>
         </div>
         
 
         <div className='flex justify-evenly  p-5 '>
-            <p>User name</p>
-            <input  className='border-black  px-3 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40' type='text' value={Username} onChange={(e)=>{setUserName(e.target.value)}}/>
+            <p className='text-white text-md font-bold text-m'>Username</p>
+            <input  className='border-black  px-3 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mr-3' type='text' value={Username} onChange={(e)=>{setUserName(e.target.value)}}/>
         </div>
 
-        <div className='flex justify-evenly p-5'>
-            <p>Password</p>
-            <input  className='border-black  px-3 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40' type='password' value={Password} onChange={(e)=>{setPassword(e.target.value)}}/>
+        <div className='flex justify-evenly  p-5'>
+            <p className='text-white text-md font-bold text-m'>Password</p>
+            <input  className='border-black  px-3 text-black-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mr-2' type='password' value={Password} onChange={(e)=>{setPassword(e.target.value)}}/>
            
         </div>
-        {/* {!isAllowed&& <p className='text-red-500'>Your credentials are wrong!!!</p>} */}
+        {!isAllowed&& <p className='text-red-500'>Your credentials are wrong!!!</p>}
 
-        {!isAllowed &&<p className='text-red-500'>Your credentials are wrong</p>}
+        {/* {!isAllowed && alert("Your credentials are wrong")} */}
+       
+        
 
        
     
 
-        <div className='flex justify-end p-5'>
-            <button onClick={()=>{fun1(IPaddress);}} className='border bg-gray-200 border-black mr-2 px-5'>Sign In</button>
-            <button  onClick={()=>{fun2()}} className='border  bg-gray-200 border-black px-5'>Cancel</button>
+        <div className='flex justify-center items-center p-5  mr-20 mt-2'>
+            <button onClick={()=>{fun1(IPaddress);}} className='bg-transparent hover:bg-blue-500 text-white ml-2 font-semibold hover:text-white py-2 px-4 border border-white-500 hover:border-transparent rounded'>Sign In</button>
+            <button  onClick={()=>{fun2()}} className='bg-transparent hover:bg-blue-500 text-white ml-2 font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded'>Cancel</button>
+            {/* <button  onClick={()=>{fun2()}} className='border  bg-gray-200 border-black px-7 rounded-md hover:border-blue-400'>Cancel</button> */}
         </div>
         
         
